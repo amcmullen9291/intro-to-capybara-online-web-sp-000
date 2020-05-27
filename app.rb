ce01a6fe3297b8bc9fd143a0f0859224cb818fc7
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 19:36:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 class Application < Sinatra::Base
   # Write your code here!
   get '/' do
+    <label for:user_name>name</label>
+    <form type="text" name="user_name" id="user_name">
+      <input type="submit">
+    </form> 
     erb :index
   end
   
