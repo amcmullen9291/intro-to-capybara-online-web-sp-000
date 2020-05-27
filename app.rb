f77617b0f0f8da95b5ac4b52384db3cd0283e562
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 19:25:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 class Application < Sinatra::Base
   # Write your code here!
   get '/' do
-    @user_name = params[:name]
+    @user_name = params[:user_name]
     erb :index
+    "Welcome #{user_name}"
   end
 end
